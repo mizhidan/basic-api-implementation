94aad538abdbb9d2b700be31db94773498721658
unknown <dj9824@126.com>
2020-08-06 21:02:10 +0800

    [danjie] feat: print log

--- src/main/java/com/thoughtworks/rslist/component/ErrorExceptionHandler.java
+++ src/main/java/com/thoughtworks/rslist/component/ErrorExceptionHandler.java
@@ -4,15 +4,22 @@ import com.thoughtworks.rslist.Exception.Error;
 import com.thoughtworks.rslist.Exception.RsEventNotValidExcption;
 import com.thoughtworks.rslist.domain.RsEvent;
 import com.thoughtworks.rslist.domain.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
-public class RsEventExceptionHandler {
 
-    @ExceptionHandler(value = {MethodArgumentNotValidException.class, RsEventNotValidExcption.class})
+
+@ControllerAdvice
+public class ErrorExceptionHandler {
+
+    private static final String logExceptionFormat = "Capture Exception By GlobalExceptionHandler - Detail: %s";
+    private static Logger log = LoggerFactory.getLogger(ErrorExceptionHandler.class);
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(value = {MethodArgumentNotValidException.class, RsEventNotValidExcption.class})
     public ResponseEntity RsEventExceptionHandle(Exception e) {
         String errorMessage = "";
         if (e instanceof  MethodArgumentNotValidException) {
@@ -29,6 +36,8 @@ public class RsEventExceptionHandler {
 
         Error error = new Error();
         error.setError(errorMessage);
+
+        log.error(String.format(logExceptionFormat, errorMessage));
 
         return ResponseEntity.badRequest().body(error);
     }
